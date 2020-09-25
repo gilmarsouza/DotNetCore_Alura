66ad4afba1ba3838b0803d4fbac3c74e7c67bf67
Gilmar Lima de Souza <gilmar.limasou@gmail.com>
2020-09-25 10:38:50 -0300

    Entity Framework Core: Banco de dados de forma eficiente (Aula 05 - Relacionamentos Um para Muitos no Entity)

---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -0,0 +1,15 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Compra
+    {
+        public int Id { get; set; }
+        public int Quantidade { get; internal set; }
+        public int ProdutoId { get; set; }
+        public Produto Produto { get; internal set; }
+        public double Preco { get; internal set; }
+        public override string ToString()
+        {
+            return $"Compra: { this.Id }, { this.Quantidade }, { this.ProdutoId }, { this.Preco }";
+        }
+    }
+}
