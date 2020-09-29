e9eab8e94d202889279e16e087e41a281e4766c2
Gilmar Lima de Souza <gilmar.limasou@gmail.com>
2020-09-29 11:27:15 -0300

    Asp.NET Core: Uma webapp usando o padrão MVC (Aula 07 - Renderização de view no AspNet Core MVC)

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/CadastroController.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/CadastroController.cs
@@ -2,9 +2,8 @@
 using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App.Logica
@@ -18,10 +17,10 @@ namespace Alura.ListaLeitura.App.Logica
             return "O livro foi adicionado com sucesso";
         }
 
-        public static Task ExibeFormulario(HttpContext context)
+        public IActionResult ExibeFormulario()
         {
-            var html = HtmlUtils.CarregaArquivoHTML("formulario");
-            return context.Response.WriteAsync(html);
+            var html = new ViewResult { ViewName = "formulario" };
+            return html;
         }
     }
 }
--- Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/LivrosController.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/LivrosController.cs
@@ -2,51 +2,44 @@
 using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using System;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App.Logica
 {
-    public class LivrosController
+    public class LivrosController : Controller
     {
         private static string CarregaLista(IEnumerable<Livro> livros)
         {
             var conteudoArquivo = HtmlUtils.CarregaArquivoHTML("lista");
 
-            foreach (var livro in livros)
-            {
-                conteudoArquivo = conteudoArquivo
-                    .Replace("#NOVO-ITEM#", $"<li>{ livro.Titulo } - { livro.Autor }</li>#NOVO-ITEM#");
-            }
-
             return conteudoArquivo.Replace("#NOVO-ITEM#", string.Empty);
         }
 
-        public static Task ParaLer(HttpContext context)
+        public IActionResult ParaLer()
         {
             var _repo = new LivroRepositorioCSV();
-            var html = CarregaLista(_repo.ParaLer.Livros);
+            ViewBag.Livros = _repo.ParaLer.Livros;
 
-            return context.Response.WriteAsync(html);
+            return View("lista");
         }
 
-        public static Task Lendo(HttpContext context)
+        public IActionResult Lendo()
         {
             var _repo = new LivroRepositorioCSV();
-            var html = CarregaLista(_repo.Lendo.Livros);
+            ViewBag.Livros = _repo.Lendo.Livros;
 
-            return context.Response.WriteAsync(html);
+            return View("lista");
         }
 
-        public static Task Lidos(HttpContext context)
+        public IActionResult Lidos()
         {
             var _repo = new LivroRepositorioCSV();
-            var html = CarregaLista(_repo.Lidos.Livros);
+            ViewBag.Livros = _repo.Lidos.Livros;
 
-            return context.Response.WriteAsync(html);
+            return View("lista");
         }
 
         public string Detalhes(int id)
