a1596552e25b340b5e6f219817fee05ce74ee5d0
Gilmar Lima de Souza <gilmar.limasou@gmail.com>
2020-10-01 14:32:22 -0300

    ASP.NET Core parte 1: Um e-Commerce com MVC e EF Core (Aula 06 - Carrinho e Resumo)

--- CasaDoCodigo/Aulas/Aula5/CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Aulas/Aula5/CasaDoCodigo/Controllers/PedidoController.cs
@@ -25,8 +25,13 @@ namespace CasaDoCodigo.Controllers
             return View(produtoRepository.GetProdutos());
         }
 
-        public IActionResult Carrinho()
+        public IActionResult Carrinho(string codigo)
         {
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                pedidoRepository.AddItem(codigo);
+            }
+
             Pedido pedido = pedidoRepository.GetPedido();
             return View(pedido.Itens);
         }
@@ -38,7 +43,8 @@ namespace CasaDoCodigo.Controllers
 
         public IActionResult Resumo()
         {
-            return View();
+            var pedido = pedidoRepository.GetPedido();
+            return View(pedido);
         }
 
     }
