9f7234b7594abc8f5425855c5bf07a55573f091d
gilmarsouza <gilmar.limasou@gmail.com>
2020-11-10 16:06:16 -0300

    ASP.NET Core Parte 5: Modularização e Componentização (Aula 03.ViewComponents)

---
+++ ASPNETCore-Parte5-master/antes/Item03/CasaDoCodigo/Areas/Catalogo/Models/ViewModels/CategoriasViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Areas.Catalogo.Models.ViewModels {
+    public class CategoriasViewModel {
+
+        public IList<Categoria> Categorias { get; set; }
+        public IList<Produto> Produtos { get; set; }
+        public int TamanhoPagina { get; set; }
+        public CategoriasViewModel(IList<Categoria> categorias, IList<Produto> produtos, int tamanhoPagina)
+        {
+            Categorias = categorias;
+            Produtos = produtos;
+            TamanhoPagina = tamanhoPagina;
+        }
+    }
+}
