90dff68ea2e8dc1c30a69c395a3405f86e9145e7
gilmarsouza <gilmar.limasou@gmail.com>
2021-07-16 08:04:57 -0300

    API Rest - Commit Inicial

---
+++ API_Rest_Dotnet_Core_5/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,32 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController: ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
+        }
+
+    }
+}
